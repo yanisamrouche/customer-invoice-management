24bf4d6233b7cbb6396907b907b8e60291184192
Lior Chamla <lchamla@gmail.com>
2022-01-14 08:12:50 +0100

    fix cypress api

--- cypress/utils.js
+++ cypress/utils.js
@@ -1,10 +1,9 @@
 // L'URL sur laquelle votre application web est visitable (à changer si nécessaire)
 export const BASE_URL = "http://localhost:4200/";
 // L'URL de l'API Supabase à mettre à jour absolument
-export const API_URL = "https://gdzprwugmxdxnpgztbwt.supabase.co/rest/v1";
+export const API_URL = "https://IDENTIFANT_SUPABASE.supabase.co/rest/v1";
 // La clé d'API de votre compte Supabase à mettre à jour absolument
-export const API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDUyMjE4MCwiZXhwIjoxOTU2MDk4MTgwfQ.bs4zHK_Hor5Z1ojmZ9MBF_jTvZpiye_DpiUPZcq209A";
+export const API_KEY = "CLE_API_SUPABASE";
 
 /**
  * Petite fonction utilitaire qui permet de supprimer tout ce qui se trouve dans les tables customers et invoices
